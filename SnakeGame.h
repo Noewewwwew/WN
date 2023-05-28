6d4c347cbfdd6fd2244bacaf8a632b578f056536
powerslam <timsort@kookmin.ac.kr>
2023-05-28 11:55:49 +0900

    #1 define 매크로 -> namespace + const 로 변경

--- SnakeGame.h
+++ SnakeGame.h
@@ -3,40 +3,46 @@
 // 맵 사이즈
 #define MAP_SIZE 21
 
-// 게임 상태 FLAG
-#define LOSE 0
-#define GAMING 1
-#define WIN 2
-
-// map 데이터 별 이름 지정
-#define BOARD 0
-#define IMMU_WALL 1
-#define WALL 2
-#define PORTAL 3
-#define SNAKE_HEAD 4
-#define SNAKE_BODY 5
-#define GROWTH_ITEM 6
-#define POISON_ITEM 7
-
 // pair를 사용할 때 first는 Y, second는 X
 #define Y first
 #define X second
 
-// SNAKE 머리 방향
-#define LEFT 0
-#define UP 1
-#define DOWN 2
-#define RIGHT 3
-
 // snake 의 머리와 몸통에 대한 정보
 typedef std::pair<int, int> pos;
+
+// 게임 상태 FLAG
+namespace GAME_STATUS {
+    const int LOSE = 0;
+    const int GAMING = 1;
+    const int WIN = 2;
+};
+
+// map 데이터 별 이름 지정
+namespace ELEMENT_KIND {
+    const int BOARD = 0;
+    const int IMMU_WALL = 1;
+    const int WALL = 2;
+    const int PORTAL = 3;
+    const int SNAKE_HEAD = 4;
+    const int SNAKE_BODY = 5;
+    const int GROWTH_ITEM = 6;
+    const int POISON_ITEM = 7;
+};
+
+// SNAKE 머리 방향
+namespace SNAKE_HEAD_DIRECTION {
+    const int LEFT = 0;
+    const int UP = 1;
+    const int RIGHT = 2;
+    const int DOWN = 3;
+};
 
 class SnakeGame {
     // 맵 데이터
     int map[MAP_SIZE][MAP_SIZE];
 
     // 게임 상태는 게임 중
-    int gameStatus = GAMING;
+    int gameStatus = GAME_STATUS::GAMING;
 
 public:
     // 생성자
@@ -49,9 +55,9 @@ public:
     }
     
     // 게임 상태가 GAMING, LOSE, WIN 인지 각각 반환
-    bool isGaming() { return getGameStatus() == GAMING; }
-    bool isLose() { return getGameStatus() == LOSE; }
-    bool isWin() { return getGameStatus() == WIN; }
+    bool isGaming() { return getGameStatus() == GAME_STATUS::GAMING; }
+    bool isLose() { return getGameStatus() == GAME_STATUS::LOSE; }
+    bool isWin() { return getGameStatus() == GAME_STATUS::WIN; }
 
     // 초기화
     void init();
