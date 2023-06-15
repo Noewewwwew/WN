9b6f3ae553ad1b403f96fe3cbd4be6723506e6bb
powerslam <timsort@kookmin.ac.kr>
2023-06-15 16:42:22 +0900

    stage 관련 코드 추가

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,21 +1,14 @@
 #include <ncurses/ncurses.h>
 #include <iostream>
 #include <windows.h>
+#include <memory.h>
 #include "SnakeGame.h"
 
 #define DEBUG false
 
 using namespace std;
 
-SnakeGame::SnakeGame(): snake(MAP_SIZE / 2, MAP_SIZE / 2), wall(MAP_SIZE) {
-    // map 외곽 초기화(WALL)
-    for(pos p: this->wall.get_wall_info())
-        setElement(p, ELEMENT_KIND::WALL);
-
-    this->wall.initPortal();
-    setElement(this->wall.getPortal1(), ELEMENT_KIND::PORTAL);
-    setElement(this->wall.getPortal2(), ELEMENT_KIND::PORTAL);
-    
+SnakeGame::SnakeGame(): snake(1, MAP_SIZE / 2), wall(MAP_SIZE, 1) {
     // 시작 상태 초기화(스테이지, 아이템)
     CurrentStage = 1;
     B = GI = PI = G = 0;
@@ -88,290 +81,82 @@ void SnakeGame::changePortal() {
 }
 
 void SnakeGame::changemap(){
-    // 게임 상태 업데이트 작업
-    // 미션 달성 체크 및 스테이지 변경 로직 구현
-        // 스테이지 넘어가는 로직
-        CurrentStage++;
-        SnakeGame::mapupdate(); // 맵 업데이트 호출
-        // 다음 스테이지 초기화 작업 등을 수행
+     // 게임 상태 업데이트 작업
+     // 미션 달성 체크 및 스테이지 변경 로직 구현
+     // 스테이지 넘어가는 로직
+     CurrentStage++;
+     SnakeGame::mapupdate(); // 맵 업데이트 호출
+     // 다음 스테이지 초기화 작업 등을 수행
 }
 
 void SnakeGame::mapupdate(){
-     switch(CurrentStage){
-          case 1:
-               // Snake 시작 위치 초기화
-               this->map[MAP_SIZE / 2][MAP_SIZE / 2] = ELEMENT_KIND::SNAKE_HEAD;
+     if(CurrentStage == 6){
+          this->setGameStatus(GAME_STATUS::WIN);
+          return;
+     }
 
-               for(int i = 1; i < 4; i++){
-                    this->map[MAP_SIZE / 2][MAP_SIZE / 2 + i] = ELEMENT_KIND::SNAKE_BODY;
-               }
-               
-               // map 외곽 초기화(IMMU_WALL)
-               this->map[0][0] = this->map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-               this->map[MAP_SIZE - 1][0] = this->map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
+     // map 초기화
+     memset(map, ELEMENT_KIND::BOARD, sizeof(map));
 
-          break;
+     this->snake = Snake(1, MAP_SIZE / 2);
 
-          case 2:
-               // map 초기화(Board)
-               for(int i = 1; i < MAP_SIZE-1; i++){
-                    for(int j = 1; j < MAP_SIZE - 1; j++){
-                         this->map[i][j] = ELEMENT_KIND::BOARD;
-                    }
-               }
+     // Snake 시작 위치 초기화
+     this->map[1][MAP_SIZE / 2] = ELEMENT_KIND::SNAKE_HEAD;
 
-               // snake 초기화
-               this->snake = Snake(MAP_SIZE / 2, MAP_SIZE / 2);
+     for(int i = 1; i < 4; i++){
+          this->map[1][MAP_SIZE / 2 + i] = ELEMENT_KIND::SNAKE_BODY;
+     }
 
-               this->map[MAP_SIZE / 2][MAP_SIZE / 2] = ELEMENT_KIND::SNAKE_HEAD;
+     this->wall = Wall(MAP_SIZE, CurrentStage);
 
-               for(int i = 1; i < 4; i++){
-                    this->map[MAP_SIZE / 2][MAP_SIZE / 2 + i] = ELEMENT_KIND::SNAKE_BODY;
-               }
+     // map 외곽 초기화(WALL)
+     for(pos p: this->wall.get_wall_info())
+          setElement(p, ELEMENT_KIND::WALL);
+     
+     this->wall.initPortal();
+     setElement(this->wall.getPortal1(), ELEMENT_KIND::PORTAL);
+     setElement(this->wall.getPortal2(), ELEMENT_KIND::PORTAL);
 
-               // map 외곽 초기화
-               this->map[0][0] = this->map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-               this->map[MAP_SIZE - 1][0] = this->map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-
-               // map 외곽 초기화(WALL)
-               for(int i = 1; i < MAP_SIZE - 1; i++){
-                    this->map[0][i] = this->map[i][0] = this->map[MAP_SIZE - 1][i] = this->map[i][MAP_SIZE - 1] = ELEMENT_KIND::WALL;    
-               }
-
-               for(int i = 0; i < 3; i++){
-                    for(int j = 0 ; j < 10; j++){
-                         this->map[4 + 4 * i][4 + j] = this->map[18 + 4 * i][4 + j] = ELEMENT_KIND::WALL;
-                         this->map[4 + 4 * i][17 + j] = this->map[18 + 4 * i][17 + j] = ELEMENT_KIND::WALL;
-                    }
-               }
-          
-               break;
-
-          case 3:
-               // map 초기화(Board)
-               for(int i = 1; i < MAP_SIZE-1; i++){
-                    for(int j = 1; j < MAP_SIZE - 1; j++){
-                         this->map[i][j] = ELEMENT_KIND::BOARD;
-                    }
-               }
-               // snake 초기화
-               this->snake = Snake(MAP_SIZE / 2, MAP_SIZE / 2);
-
-               // Snake 시작 위치 초기화
-               this->map[MAP_SIZE / 2][MAP_SIZE / 2] = ELEMENT_KIND::SNAKE_HEAD;
-
-               for(int i = 1; i < 4; i++){
-                    this->map[MAP_SIZE / 2][MAP_SIZE / 2 + i] = ELEMENT_KIND::SNAKE_BODY;
-               }
-
-               // map 외곽 초기화(IMMU_WALL)
-               map[0][0] =  map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-               map[MAP_SIZE - 1][0] =  map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-
-               // map 외곽 초기화(WALL)
-               for(int i = 1; i < MAP_SIZE - 1; i++){
-                    map[0][i] =  map[i][0] =  map[MAP_SIZE - 1][i] =  map[i][MAP_SIZE - 1] = ELEMENT_KIND::WALL;
-               }
-
-               
-               for(int i = 1; i <= 3 ; i++ ){
-                    for(int j = 1; j <= 3; j++){
-                         map[8 * i + i -1][8 * j + j -1] =  map[8 * i + i][8 * j + j -1] = ELEMENT_KIND::WALL;
-                         map[8 * i + i -1][8 * j + j] =  map[8 * i + i][8 * j + j] = ELEMENT_KIND::WALL;
-                    }
-               }
-
-               for(int i = 1; i <= 3 ; i++ ){
-                    for(int j = 0; j < 3; j++){
-                         map[8 * i + i -1][3 * (3 * j + 1)] =  map[8 * i + i - 1][3 * (3 * j + 1) + 1] = ELEMENT_KIND::WALL;
-                         map[8 * i + i][3 * (3 * j + 1)] =  map[8 * i + i][3 * (3 * j + 1) + 1] = ELEMENT_KIND::WALL;
-                    }
-               }
-
-               for(int i = 0; i < 3; i++ ){
-                    for(int j = 0; j < 3; j++){
-                         map[3 * (3 * i + 1) ][3 * (3 * j + 1)] =  map[3 * (3 * i + 1)][3 * (3 * j + 1) + 1] = ELEMENT_KIND::WALL;
-                         map[3 * (3 * i + 1) + 1][3 * (3 * j + 1)] =  map[3 * (3 * i + 1) + 1][3 * (3 * j + 1) + 1] = ELEMENT_KIND::WALL;
-                    }
-               }
-
-               for(int i = 0; i < 3; i++ ){
-                    for(int j = 1; j <= 3; j++){
-                         map[3 * (3 * i + 1)][8 * j + j -1] =  map[3 * (3 * i + 1)][8 * j + j] = ELEMENT_KIND::WALL;
-                         map[3 * (3 * i + 1) + 1][8 * j + j -1] =  map[3 * (3 * i + 1) + 1][8 * j + j] = ELEMENT_KIND::WALL;
-                    }
-               }
-          break;
-
-          case 4:
-               // map 초기화(Board)
-               for(int i = 1; i < MAP_SIZE-1; i++){
-                    for(int j = 1; j < MAP_SIZE - 1; j++){
-                         this->map[i][j] = ELEMENT_KIND::BOARD;
-                    }
-               }
-
-               // snake 초기화
-               this->snake = Snake(1, 15);
-
-               this->map[1][15] = ELEMENT_KIND::SNAKE_HEAD;
-
-               for(int i = 1; i < 3; i++){
-                    this->map[1][15 + i] = ELEMENT_KIND::SNAKE_BODY;
-               }
-
-               // map 외곽 초기화(WALL)
-               this->map[0][0] = this->map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-               this->map[MAP_SIZE - 1][0] = this->map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-
-               // map 외곽 초기화(WALL)
-               for(int i = 1; i < MAP_SIZE - 1; i++){
-                    map[0][i] =  map[i][0] =  map[MAP_SIZE - 1][i] =  map[i][MAP_SIZE - 1] = ELEMENT_KIND::WALL;
-               }
-
-               //가로1
-               for(int i = 0; i < 23; i++){
-                    map[4][4 + i] =  map[26][4 + i] = ELEMENT_KIND::WALL;
-               }
-               
-                    //가로2
-               for(int i = 0; i < 11; i++){
-                    map[8][8 + i] =  map[22][12 + i] = ELEMENT_KIND::WALL;
-                    map[18][8 + i] =  map[12][12 + i] = ELEMENT_KIND::WALL;
-               }
-
-                    //세로1
-               for(int i = 0; i < 18; i++){
-                    map[8 + i][4] =  map[4 + i][26] = ELEMENT_KIND::WALL;
-               }
-
-               //세로1
-               for(int i = 0; i < 14; i++){
-                    map[9 + i][8] =  map[8 + i][22] = ELEMENT_KIND::WALL;
-               }
-
-               //세로1
-               for(int i = 0; i < 3; i++){
-                    map[13 + i][12] =  map[15 + i][18] = ELEMENT_KIND::WALL;
-               }
-          break;
-          
-          case 5:
-               // map 초기화(Board)
-               for(int i = 1; i < MAP_SIZE-1; i++){
-                    for(int j = 1; j < MAP_SIZE - 1; j++){
-                         this->map[i][j] = ELEMENT_KIND::BOARD;
-                    }
-               }
-
-               // snake 초기화
-               this->snake = Snake(1, 15);
-
-               map[1][15] = ELEMENT_KIND::SNAKE_HEAD;
-
-               for(int i = 1; i < 3; i++){
-                    map[1][15 + i] = ELEMENT_KIND::SNAKE_BODY;
-               }
-               
-               // map 외곽 초기화(IMMU_WALL)
-                    map[0][0] =  map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-                    map[MAP_SIZE - 1][0] =  map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
-
-               // map 외곽 초기화(WALL)
-               for(int i = 1; i < MAP_SIZE - 1; i++){
-                    map[0][i] =  map[i][0] =  map[MAP_SIZE - 1][i] =  map[i][MAP_SIZE - 1] = ELEMENT_KIND::WALL;
-               }
-                    //S
-               for(int i = 0; i < 4; i++){
-                    map[2][2 + i] =  map[8][2 + i] = ELEMENT_KIND::WALL;
-               }
-               for(int i = 0; i < 3; i++){
-                    map[3 + i][2] =  map[5 + i][5] = ELEMENT_KIND::WALL; 
-               }
-                    map[5][4] = ELEMENT_KIND::WALL;
-
-               //N
-               for(int i = 0; i < 7; i++){
-                    map[2 + i][7] =  map[2 + i][12] =  ELEMENT_KIND::WALL;
-               }
-
-               for(int i = 0; i < 2; i++){
-                    map[2 + i][8] =  map[6 + i][10] =  ELEMENT_KIND::WALL;
-                    map[3 + i][9] =  map[7 + i][11] =  ELEMENT_KIND::WALL;
-               }
-
-               //A
-               for(int i = 0; i < 7; i++){
-                    map[2 + i][14] =  map[2 + i][18] =  ELEMENT_KIND::WALL;
-                    map[22 + i][9] =  map[22 + i][13] =  ELEMENT_KIND::WALL;
-               }
-
-               for(int i = 0; i < 3; i++){
-                    map[2][15 + i] =  map[22][10 + i] =  ELEMENT_KIND::WALL;
-               }
-                    map[5][16] =  map[25][11] =  ELEMENT_KIND::WALL;
-
-               //K
-               for(int i = 0; i < 3; i++){
-                    map[2 + i][23 - i] =  map[6 + i][21 + i] = ELEMENT_KIND::WALL;
-                    map[2 + i][20] =  map[6 + i][20] =ELEMENT_KIND::WALL;
-               }    
-
-               //E
-               for(int i = 0; i < 4; i++){
-                    map[2][25 + i] =  map[5][25 + i] =  map[8][25 + i] = ELEMENT_KIND::WALL;
-                    map[22][25 + i] =  map[25][25 + i] =  map[28][25 + i] = ELEMENT_KIND::WALL;
-               }
-
-               for(int i = 0; i < 2; i++){
-                    map[3 + i][25] =  map[6 + i][25] = ELEMENT_KIND::WALL;
-                    map[23 + i][25] =  map[26 + i][25] = ELEMENT_KIND::WALL;
-               }
-
-               //G
-               for(int i = 0; i < 6; i++){
-                    map[22][2 + i] =  map[28][2 + i] = ELEMENT_KIND::WALL;
-               }
-               
-               for(int i = 0; i < 5; i++){
-                    map[23 + i][2] = ELEMENT_KIND::WALL;
-               }
-
-                    map[23][7] =  map[27][7] =  map[25][5] = ELEMENT_KIND::WALL;
-                    map[25][6] =  map[25][7] =  map[26][5] = ELEMENT_KIND::WALL;
-
-               //M
-               for(int i = 0; i < 7; i++){
-                    map[22 + i][15] =  map[22 + i][23] =  ELEMENT_KIND::WALL;
-               }
-
-                    map[23][16] =  map[23][22] =  map[25][17] =  map[25][21] = ELEMENT_KIND::WALL;
-                    map[27][18] =  map[27][19] =  map[27][20] =  map[28][19] = ELEMENT_KIND::WALL;
-
-               //SNAKE
-               for(int i = 0; i < 3; i++){
-                    for(int j = 0; j < 9; j++){
-                         map[10 + i][2 + j] =  map[10 + i][14 + j] = ELEMENT_KIND::WALL;
-                         map[18 + i][8 + j] =  map[18 + i][20 + j] = ELEMENT_KIND::WALL;
-                    }
-               }
-
-               for(int i = 0; i < 11; i++){
-                    for(int j = 0; j < 3; j++){
-                         map[10 + i][2 + j] =  map[10 + i][14 + j] = ELEMENT_KIND::WALL;
-                         map[10 + i][8 + j] =  map[10 + i][20 + j] = ELEMENT_KIND::WALL;
-                         map[10 + i][26 + j] = ELEMENT_KIND::WALL;
-                    }
-               }
-
-               for(int i = 0; i < 3; i++){
-                    map[17][8 + i] =  map[13][20 + i] = ELEMENT_KIND::BOARD;
-               }
-          break;
-          
-          default:
-               this->gameStatus = GAME_STATUS::WIN;
+     // map 외곽 초기화(IMMU_WALL)
+     this->map[0][0] = this->map[0][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
+     this->map[MAP_SIZE - 1][0] = this->map[MAP_SIZE - 1][MAP_SIZE - 1] = ELEMENT_KIND::IMMU_WALL;
+     if(CurrentStage == 5){
+          for(int i = 11; i < 20; i++){
+               this->map[i][3] = ELEMENT_KIND::IMMU_WALL;
           }
+
+          for(int i = 3; i < 10; i++){
+               this->map[11][i] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 11; i < 15; i++){
+               this->map[i][9] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 9; i < 16; i++){
+               this->map[19][i] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 11; i < 19; i++){
+               this->map[i][15] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 15; i < 22; i++){
+               this->map[11][i] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 15; i < 19; i++){
+               this->map[i][21] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 21; i < 28; i++){
+               this->map[19][i] = ELEMENT_KIND::IMMU_WALL;
+          }
+
+          for(int i = 11; i < 19; i++){
+               this->map[i][27] = ELEMENT_KIND::IMMU_WALL;
+          }
+     }
 }
 
 void SnakeGame::update(){
